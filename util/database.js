4d1884587517872ea46a83372f559a657d06586a
wdilermando <willamisdesign@gmail.com>
2024-01-18 11:51:22 -0300

    set

--- util/database.js
+++ util/database.js
@@ -3,27 +3,25 @@ const MongoClient = mongodb.MongoClient;
 
 let _db;
 
-const mongoConnect = callback =>{
-
-    MongoClient.connect('mongodb+srv://will:4Jsf62SldPWjg3dP@atlabox-nkckf.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
-    .then(client =>{
-        console.log('Connected!');  
-        _db = client.db()  
-        callback();
+const mongoConnect = callback => {
+  MongoClient.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true })
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
     })
-    .catch(err =>{
-        console.log(err); 
-        throw err;   
-    })
-}
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
 
-const getDb = () =>{
-    if(_db) {
-        return _db;
-    }
-    throw 'No database found!'
-}
-
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
 
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
